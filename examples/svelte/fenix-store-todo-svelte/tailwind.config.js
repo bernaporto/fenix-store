278b1fa7f6735f81b487ea9205cbc90faec73288
Bernardo Porto <porto.bernardo@gmail.com>
2024-05-09 21:06:13 +0200

    feat(examples): add todo svelte project

---
+++ examples/svelte/fenix-store-todo-svelte/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+import { theme } from '../../tailwind.config';
+
+export default {
+  theme,
+  darkMode: 'selector',
+  content: ['./src/**/*.{ts,tsx}', './*.html'],
+};
