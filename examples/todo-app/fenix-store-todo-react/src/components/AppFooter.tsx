cfc3a3426739186e68cfff56d2af0154d5a56c31
Bernardo Porto <porto.bernardo@gmail.com>
2024-05-09 21:08:38 +0200

    chore: change examples folder structure

--- examples/todo-app/fenix-store-todo-react/src/components/AppFooter.tsx
+++ examples/todo-app/fenix-store-todo-react/src/components/AppFooter.tsx
@@ -1,7 +1,7 @@
 export const AppFooter: React.FC = () => {
   return (
     <footer className="text-center text-gray-600 dark:text-gray-400 py-4">
-      <i className="bi bi-github mr-1" />{' '}
+      <i className="bi bi-github" />{' '}
       <a
         href="https://github.com/bernaporto/fenix-store"
         rel="noopener noreferrer"
--- examples/todo-app/fenix-store-todo-svelte/tailwind.config.js
+++ examples/todo-app/fenix-store-todo-svelte/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import { theme } from '../../tailwind.config';
+import { theme } from '../shared/tailwind.config';
 
 export default {
   theme,
